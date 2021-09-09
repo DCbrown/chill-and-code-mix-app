e5bbe0410e530955f25d8a3cefed4bec7f6a810d
dcbrown <browncdonovan@gmail.com>
2021-09-08 21:30:52 -0400

    add music - flamingosis, sfex3, mm, oneups

--- demo/src/index.js
+++ demo/src/index.js
@@ -25,6 +25,97 @@ const songs = [
       song: 'Street Fighter II Sagat Theme',
     },
   },
+  {
+    url: '../../src/playlist/CrashMan From The OneUps.mp3',
+    artist: {
+      name: 'The OneUps',
+      song: 'Crash Man',
+    },
+  },
+  {
+    url: '../../src/playlist/Fire Drill.mp3',
+    artist: {
+      name: 'Gecko Yamori',
+      song: 'Fire Drill',
+    },
+  },
+  {
+    url: '../../src/playlist/Flash the Funk.mp3',
+    artist: {
+      name: 'Gecko Yamori',
+      song: 'Flash the Funk',
+    },
+  },
+  {
+    url: '../../src/playlist/Street Fighter EX3 Music Ancient Zone Theme.mp3',
+    artist: {
+      name: 'CAPCOM',
+      song: 'Street Fighter EX3 Music Ancient Zone',
+    },
+  },
+  {
+    url: '../../src/playlist/Street Fighter EX3 OST - Character Edit.mp3',
+    artist: {
+      name: 'CAPCOM',
+      song: 'Street Fighter EX3 OST - Character Edit',
+    },
+  },
+  {
+    url: '../../src/playlist/The OneUps - F-Zero - Port Authority [Port Town].mp3',
+    artist: {
+      name: 'The OneUps',
+      song: 'F-Zero - Port Authority [Port Town]',
+    },
+  },
+  {
+    url: '../../src/playlist/The OneUps - F-Zero - Silence is Golden, Snitches Get Stitches [Silence].mp3',
+    artist: {
+      name: 'The OneUps',
+      song: 'F-Zero - Silence is Golden',
+    },
+  },
+  {
+    url: '../../src/playlist/The OneUps - Mega Man X - Boomer Kuwanger.mp3',
+    artist: {
+      name: 'The OneUps',
+      song: 'Mega Man X - Boomer Kuwanger',
+    },
+  },
+  {
+    url: '../../src/playlist/The OneUps - ToeJam & Earl - Alien Break Down.mp3',
+    artist: {
+      name: 'The OneUps',
+      song: 'ToeJam & Earl - Alien Break Down',
+    },
+  },
+  {
+    url: '../../src/playlist/Flamingosis - A Groovy Thing - 15 get yourself together.mp3',
+    artist: {
+      name: 'Flamingosis',
+      song: 'Get Yourself Together',
+    },
+  },
+  {
+    url: '../../src/playlist/Flamingosis - Come & Get It feat. Yung Bae (2017).mp3',
+    artist: {
+      name: 'Flamingosis',
+      song: 'Come & Get It feat. Yung Bae',
+    },
+  },
+  {
+    url: '../../src/playlist/Flamingosis - Feelings of sentimentality due to getting curved.mp3',
+    artist: {
+      name: 'Flamingosis',
+      song: 'Feelings Of Sentimentality Due To Getting Curved',
+    },
+  },
+  {
+    url: '../../src/playlist/Flamingosis - Flute Salad (2017).mp3',
+    artist: {
+      name: 'Flamingosis',
+      song: 'Flute Salad',
+    },
+  },
 ];
 
 ReactDOM.render(
